4778cb982c07bc9a3f5b3340e40b305c74fdff10
Joseph George <joey@grabcad.com>
2013-07-17 14:27:28 -0400

    remove Savon 1.x style configuration for compatbility with Savon 2.x

--- lib/marketo.rb
+++ lib/marketo.rb
@@ -1,10 +1,6 @@
 require 'rubygems'
 require 'savon'
 
-Savon.configure do |config|
-  config.log = false # disable logging
-end
-
 require File.expand_path('marketo/client', File.dirname(__FILE__))
 require File.expand_path('marketo/authentication_header', File.dirname(__FILE__))
 require File.expand_path('marketo/enums', File.dirname(__FILE__))
