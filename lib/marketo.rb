89843ef94534af6f72cd84deda122cd3d4388c58
Joseph George <joey@grabcad.com>
2013-07-29 14:46:35 -0400

    add lead type

--- lib/marketo.rb
+++ lib/marketo.rb
@@ -4,7 +4,9 @@ require 'savon'
 require File.expand_path('marketo/client', File.dirname(__FILE__))
 require File.expand_path('marketo/authentication_header', File.dirname(__FILE__))
 require File.expand_path('marketo/enums', File.dirname(__FILE__))
+require File.expand_path('marketo/key', File.dirname(__FILE__))
 require File.expand_path('marketo/lead_key', File.dirname(__FILE__))
+require File.expand_path('marketo/list_key', File.dirname(__FILE__))
 require File.expand_path('marketo/lead_record', File.dirname(__FILE__))
 
 
