d70e0d2365db015b609f166bf5dd03e053cc849e
Austin Ziegler <austin@clearfit.com>
2014-08-01 13:26:20 -0400

    New integration tests.

--- test/integration/test_leads.rb
+++ test/integration/test_leads.rb
@@ -5,6 +5,7 @@ if ENV['MARKETO_USER_ID']
   begin
     verbose, $VERBOSE = $VERBOSE, nil
     require 'debugger'
+  rescue LoadError
   ensure
     $VERBOSE = verbose
   end
@@ -25,14 +26,8 @@ if ENV['MARKETO_USER_ID']
       def setup
         super
         @subject = @client.leads
-        @email   = "#{$run_id}@integration.test"
-      end
-
-      def test_01_lead_is_missing
-        assert_nil subject.get_by_email(@email)
-        assert subject.error?
-
-        expected = {
+        @email   = "bit#{$run_id}+integration@clearfit.org"
+        @fault = {
           :fault => {
             :faultcode   => 'SOAP-ENV:Client',
             :faultstring => '20103 - Lead not found',
@@ -46,7 +41,13 @@ if ENV['MARKETO_USER_ID']
             }
           }
         }
-        assert_equal expected, subject.error.to_hash
+      end
+
+      def test_01_lead_is_missing
+        assert_nil subject.get_by_email(@email)
+        assert subject.error?
+
+        assert_equal @fault, subject.error.to_hash
       end
 
       def test_02_create_lead
@@ -57,6 +58,52 @@ if ENV['MARKETO_USER_ID']
         refute_nil lead.sync!
         refute_nil lead.id
       end
+
+      def test_03_load_lead
+        lead = subject.get_by_email(@email)
+
+        refute_nil lead
+        refute_nil lead.id
+        assert_equal 'George', lead[:FirstName]
+        assert_equal @email, lead[:Email]
+      end
+
+      def test_04_change_lead_name
+        george = subject.get_by_email(@email)
+        george[:FirstName] = 'Ursula'
+        george[:LastName]  = 'Stanhope'
+
+        ursula = george.sync
+        refute_equal george.object_id, ursula.object_id
+        assert_equal george.id, ursula.id
+        assert_equal george[:FirstName], ursula[:FirstName]
+        assert_equal george[:LastName], 'Stanhope'
+
+        assert_equal subject.get_by_email(@email)[:LastName], 'Stanhope'
+      end
+
+      def test_05_change_lead_email
+        lead = subject.get_by_email(@email)
+        email = @email.gsub(/\.org/, '.com')
+        lead[:Email] = email
+
+        lead2 = lead.sync
+        refute_equal lead.object_id, lead2.object_id
+        assert_equal lead.id, lead2.id
+        assert_equal lead[:FirstName], lead2[:FirstName]
+        assert_equal lead[:LastName], 'Stanhope'
+        assert_equal lead2[:Email], email
+
+        assert_equal subject.get_by_email(email)[:LastName], 'Stanhope'
+
+        test_01_lead_is_missing
+
+        lead[:Email] = @email
+
+        refute_nil lead.sync!
+      end
     end
   end
+else
+  puts 'No MARKETO_USER_ID'
 end
